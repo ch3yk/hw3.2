f2982297b2dd19c81ae5ee0d71aa10ac703ea420
bodig <bodiga.stas@mail.ru>
2023-06-05 20:33:47 +0300

    все

--- src/main/java/pro/sky/calc/CalcServes.java
+++ src/main/java/pro/sky/calc/CalcServes.java
@@ -1,4 +1,13 @@
 package pro.sky.calc;
 
 public interface CalcServes {
+    String hello();
+
+    String plus(int num1, int num2);
+
+    String minus(int num1, int num2);
+
+    String multiply(int num1, int num2);
+
+    String divide(int num1, int num2);
 }
