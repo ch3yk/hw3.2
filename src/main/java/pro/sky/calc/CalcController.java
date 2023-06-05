dd2cb4ce4c914a3e5e19757e6823f3588ec6b0de
bodig <bodiga.stas@mail.ru>
2023-06-05 20:32:42 +0300

    все

--- src/main/java/pro/sky/calc/CalcController.java
+++ src/main/java/pro/sky/calc/CalcController.java
@@ -1,4 +1,35 @@
 package pro.sky.calc;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calc")
 public class CalcController {
+    private final CalcServes calcServes;
+    public CalcController(CalcServes calcServes) {
+        this.calcServes = calcServes;
+    }
+    @GetMapping
+    public String hello() {
+        return calcServes.hello();
+    }
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
+        return calcServes.plus(num1,num2);
+    }
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return calcServes.minus(num1,num2);
+    }
+    @GetMapping(path = "/multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return calcServes.multiply(num1,num2);
+    }
+    @GetMapping(path = "/divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        return calcServes.divide(num1,num2);
+    }
 }
--- src/main/java/pro/sky/calc/CalcServesImpl.java
+++ src/main/java/pro/sky/calc/CalcServesImpl.java
@@ -1,4 +1,36 @@
 package pro.sky.calc;
 
-public class CalcServesImpl {
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalcServesImpl implements CalcServes {
+@Override
+    public String hello() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+
+    @Override
+    public String plus(int num1, int num2) {
+        return num1+"+"+num2+"="+(num1+num2);
+    }
+
+    @Override
+    public String minus(int num1, int num2) {
+        return num1+"-"+num2+"="+(num1-num2);
+    }
+
+    @Override
+    public String multiply(int num1, int num2) {
+        return num1+"*"+num2+"="+(num1*num2);
+    }
+
+    @Override
+    public String divide(int num1, int num2) {
+    if (num2 == 0) {
+        return "Делить на 0 нельзя!";
+    } else {
+        return num1 + "/" + num2 + "=" + (num1 / num2);
+    }
+    }
 }
